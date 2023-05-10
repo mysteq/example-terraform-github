6574230b99b5410d2e1f17093ccfdcbe320f32ae
Ketil Gjerde <raideer@gmail.com>
2023-05-10 14:27:57 +0200

    Updated with example code
    Signed-off-by: Ketil Gjerde <raideer@gmail.com>

--- main.tf
+++ main.tf
@@ -3,10 +3,4 @@ resource "github_repository" "example" {
   description = "My awesome codebase"
 
   visibility = "public"
-
-  template {
-    owner                = "github"
-    repository           = "terraform-template-module"
-    include_all_branches = true
-  }
 }
