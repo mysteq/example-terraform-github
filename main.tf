34163c9fb405f14287aa2dc996da18e16ed83218
Ketil Gjerde <raideer@gmail.com>
2023-05-11 11:15:57 +0200

    Updated with example code
    Signed-off-by: Ketil Gjerde <raideer@gmail.com>

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "github_repository" "example" {
-  name        = "example-terraform-github-created-new"
+  name        = "example-terraform-github-created"
   description = "My awesome codebase"
 
   visibility = "public"
