0a7ee1b2c0532fdf22beac1f0a8a0a2c8c4a1327
Ketil Gjerde <raideer@gmail.com>
2023-08-05 19:42:30 +0200

    build: update
    Signed-off-by: Ketil Gjerde <raideer@gmail.com>

--- providers.tf
+++ providers.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "tfstategithub"
+    resource_group_name  = "rg-tfstate-01"
     storage_account_name = "tfstategithub3453"
     container_name       = "tfstategithub"
     key                  = "github.tfstate"
